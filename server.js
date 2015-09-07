f3c5c9ba018aaa4c0553230e3a74d788f5a447a1
James MacAdie (server name) <james@macadie.co.uk>
2015-09-07 12:19:13 +0100

    Clean up server.js file
    Had some uneeded references from earlier refactorings

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 var express = require('express')
-var bodyParser= require('body-parser')
-var Post = require('./models/post')
-
 var app = express()
-app.use(bodyParser.json())
 
 app.use('/', require('./controllers/static'))
 app.use('/api/posts', require('./controllers/api/posts'))
