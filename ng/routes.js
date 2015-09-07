81176b2d9b4245c33160bc82db2eb390df7f4a60
James MacAdie (server name) <james@macadie.co.uk>
2015-09-07 13:07:34 +0100

    Initial login functionality
    Can now log in to website (if user already created on db). Have also provided
    the api to create new users but can only access it via curl at the mo

--- ng/routes.js
+++ ng/routes.js
@@ -2,5 +2,6 @@ angular.module('app')
 .config(function($routeProvider) {
   $routeProvider
   .when('/', { controller: 'PostsCtrl', templateUrl: 'posts.html' })
-  .when('/register', { controller: 'LoginCtrl', templateUrl: 'login.html' })
+  .when('/register', { controller: 'RegisterCtrl', templateUrl: 'register.html' })
+  .when('/login', { controller: 'LoginCtrl', templateUrl: 'login.html' })
 })
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express')
-var app = express()
+var app = require('express')()
+app.use(require('body-parser').json())
 
 app.use('/', require('./controllers/static'))
 app.use('/api/posts', require('./controllers/api/posts'))
+app.use('/api/sessions', require('./controllers/api/sessions'))
+app.use('/api/users', require('./controllers/api/users'))
 
 app.listen(5000, function() {
   console.log('Server listenting on', 5000)
