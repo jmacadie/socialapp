93d1268c007209e4bddcd49e5043305f9b83ce76
James MacAdie (server name) <james@macadie.co.uk>
2015-09-07 13:54:11 +0100

    Enabled HTML5 Pushstate
    With this switched on we can mimic real life URLs on the client end

--- controllers/static.js
+++ controllers/static.js
@@ -4,8 +4,12 @@ var router = express.Router()
 router.use(express.static(__dirname + '/../assets'))
 router.use(express.static(__dirname + '/../templates'))
 
-router.get('/', function(req, res) {
+sendApp = function(req, res) {
   res.sendFile('layouts/app.html', { root: __dirname + '/..' })
-})
+}
+
+router.get('/', sendApp)
+router.get('/register', sendApp)
+router.get('/login', sendApp)
 
 module.exports = router
