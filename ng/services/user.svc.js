4e503dcc1511eddd99b346d6f8831c56ae833357
James MacAdie (server name) <james@macadie.co.uk>
2015-09-09 11:16:39 +0100

    Removed reference to this in User Service
    I originally included this following the book but it doesn't seem to be needed
    so have dropped it

--- ng/services/user.svc.js
+++ ng/services/user.svc.js
@@ -1,41 +1,38 @@
 angular.module('app')
 .factory('UserSvc', function($http) {
-  var _this = this
-
-  _this.getUser = function() {
+  function getUser() {
     return $http.get('/api/users')
   }
 
-  _this.createUser = function(username, password) {
+  function createUser(username, password) {
     return $http.post('/api/users', {
       username: username,
       password: password,
     })
     .then(function() {
-      return _this.login(username, password)
+      return login(username, password)
     })
   }
 
-  _this.login = function(username, password) {
+  function login(username, password) {
     return $http.post('/api/sessions', {
       username: username,
       password: password,
     })
     .then(function(val) {
-      _this.token = val.data
       $http.defaults.headers.common['X-Auth'] = val.data
-      return _this.getUser()
+      return getUser()
     })
   }
 
-  _this.logout = function() {
+  function logout() {
     delete $http.defaults.headers.common['X-Auth']
   }
 
   return {
-    getUser: _this.getUser,
-    createUser: _this.createUser,
-    login: _this.login,
-    logout: _this.logout,
+    getUser: getUser,
+    createUser: createUser,
+    login: login,
+    logout: logout,
   }
 })
